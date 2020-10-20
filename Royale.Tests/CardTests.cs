66e060d164018bd2dc499825cfee1d210c9feab5
Jessica <jeh_criss@hotmail.com>
2020-10-20 20:59:43 -0300

    added firt test

--- Royale.Tests/CardTests.cs
+++ Royale.Tests/CardTests.cs
@@ -1,5 +1,7 @@
+using System.IO;
 using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Royale.Tests
 {
@@ -10,14 +12,21 @@ namespace Royale.Tests
         [SetUp]
         public void BeforeEach()
         {
+            var path = Path.GetFullPath(@"../../../../" + "_drivers");
+            driver = new ChromeDriver(path);
         }
 
         [Test]
         public void Ice_Spirit_is_on_Cards_Page()
         {
-            Assert.Pass();
+            // 1. go to statsroyale.com and check if ice spirit is displayed
+            driver.Url = "https://statsroyale.com";
+            driver.FindElement(By.CssSelector("a[href='/cards']")).Click();
+            var iceSpirit = driver.FindElement(By.CssSelector("a[href*='Ice+Spirit']"));
+            Assert.That(iceSpirit.Displayed);
         }
 
+        [TearDown]
         public void AfterEach() => driver.Quit();
     }
 }
